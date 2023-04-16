6d5479992fbf9739b850f5a482ba6706030ce5e7
Olga Yarosh <O_ll_y_a@mail.ru>
2023-04-16 22:29:43 +0300

    создание файла main.py

--- main.py
+++ main.py
@@ -1,24 +1,54 @@
-from src.item import HH, Vacancy, SJ, JSONDump, USER
-
-user = USER()
-
-if user.platform == 'HH':
-    vac = HH()
-    js = JSONDump(response=vac.api())
-    js.dump_js()
-elif user.platform == 'SJ':
-    vac = SJ()
-    js = JSONDump(response=vac.api())
-    js.dump_js()
-else:
-    print('Такой платформы нет')
+from src.item import HH, SJ, Vacancy, JSONDump
+from src.user_class import USER
 
 
+def main():
+    platform = input('Введите на какой платформе выполнить поиск (HH / SJ): ')
+    keyword = input('Введите поисковый запрос: ')
+    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
+    payment_min = int(input('Введите минимальный уровень зп: '))
+    user = USER(platform, keyword, top_n, payment_min)
+    json_saver = JSONDump(user.keyword)
+    if user.platform == 'HH':
+        hh_api = HH()
+        hh_vacancies = hh_api.get_vacancies(user.keyword)
+        json_saver.add_vacancy(hh_vacancies)
+        data = json_saver.selected_hh(user.top_n, user.payment_min)
+        data = json_saver.sorted_vac_min(data)
+        for row in data:
+            # # Создание экземпляра класса для работы с вакансиями
+            # info = row.split('\n')
+            # vacancy = Vacancy(info)
+            print(row, end='\n\n')
+    elif user.platform == 'SJ':
+        superjob_api = SJ()
+        superjob_vacancies = superjob_api.get_vacancies(user.keyword)
+        json_saver.add_vacancy(superjob_vacancies)
+        data = json_saver.selected_sj(user.top_n)
+        data = json_saver.sorted_vac_min(data)
+        for row in data:
+            # # Создание экземпляра класса для работы с вакансиями
+            # info = row.split('\n')
+            # vacancy = Vacancy(info)
+            print(row, end='\n\n')
+    else:
+        print('Нет данных под ваш запрос.')
+
+# Получение вакансий с разных платформ
 
 
 
-# py = Vacancy()
-# print(py.title)
-# print(py.payment)
-# print(py.requirement)
-# print(py.responsibility)
+# # Сохранение информации о вакансиях в файл
+
+
+
+
+
+
+
+    # json_saver.delete_vacancy(data, 78473911)
+
+
+
+if __name__ == "__main__":
+    main()
