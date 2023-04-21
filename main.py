72dfdc5b13a8fc0369a86d17cd7c9d4f35bc0cb3
Olga Yarosh <O_ll_y_a@mail.ru>
2023-04-21 15:44:17 +0300

    добавление условий на заданные условия пользователя

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from src.user_class import USER
 def main():
     platform = input('Введите на какой платформе выполнить поиск (HH / SJ): ')
     keyword = input('Введите поисковый запрос: ')
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    payment_min = int(input('Введите минимальный уровень зп: '))
+    top_n = input("Введите количество вакансий для вывода в топ N: ")
+    payment_min = input('Введите минимальный уровень зп: ')
 
     user = USER(platform, keyword, top_n, payment_min)
     json_saver = JSONDump(user.keyword)
@@ -15,7 +15,11 @@ def main():
         hh_api = HH()
         hh_vacancies = hh_api.get_vacancies(user.keyword)
         json_saver.add_vacancy(hh_vacancies)
-        data = json_saver.selected_hh(user.top_n, user.payment_min)
+        data = json_saver.get_vacancies()
+        if payment_min != '':
+            data = json_saver.selected_payment_min_hh(data, payment_min)
+        if top_n != '':
+            data = json_saver.selected_top_hh(data, user.top_n)
         data = json_saver.sorted_vac_min(data)
 
         for row in data:
